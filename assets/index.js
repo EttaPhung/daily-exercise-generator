61799dbe2b76c7b9b1f9bd45ed0e48ccb36d9830
Etta Phung <99515145+EttaPhung@users.noreply.github.com>
2022-12-05 20:10:44 -0800

    Increase number, change words, add links

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 console.log("index.js is linked!")
 
 //pointer pointing to number of exercise selection
-const selection = document.querySelector('.dropdown-menu');
+const selection = document.querySelector('.dropdown-menu-select');
 //pointer pointing to answer section of html
 const answer = document.querySelector('.answer-text');
 
@@ -9,21 +9,21 @@ const answer = document.querySelector('.answer-text');
 const exercise = () => {
     const exercise = Math.floor(Math.random() * 7);
     if (exercise === 0) {
-        return 'Pushups';
+        return '<a href="https://www.hybridcalisthenics.com/pushups/" target="_blank">Pushups</a>';
     } else if (exercise === 1) {
-        return 'Leg Raises';
+        return '<a href="https://www.hybridcalisthenics.com/legraises" target="_blank">Leg Raises</a>';
     } else if (exercise === 2) {
-        return 'Squats';
+        return '<a href="https://www.hybridcalisthenics.com/squats" target="_blank">Squats</a>';
     } else if (exercise === 3) {
-        return 'Pullups';
+        return '<a href="https://www.hybridcalisthenics.com/pullups" target="_blank">Pullups</a>';
     } else if (exercise === 4) {
-        return 'Bridges';
+        return '<a href="https://www.hybridcalisthenics.com/bridges" target="_blank">Bridges</a>';
     } else if (exercise === 5) {
-        return 'Stairs Steps'
+        return '<a href="https://www.realsimple.com/health/fitness-exercise/stairs-workout">Stair Steps</a>';
     } else if (exercise === 6) {
-        return 'Jumping Jacks'
+        return '<a href="https://www.healthline.com/health/fitness-exercise/jumping-jacks#how-to">Jumping Jacks</a>'
     } else if (exercise === 7) {
-        return 'Hula Hoops'
+        return '<a href="https://us.myprotein.com/thezone/training/hula-hoop-workout-for-killer-abs-exercises-benefits-and-tips/">Jumping Jacks</a>'
     }
 };
 
@@ -35,6 +35,8 @@ const reps = () => {
 
 //function to generate exercise and the set number
 const set = (data) => {
+    console.log(data);
+    console.log(data.target);
     let number = data.target.value;
     if (number === 0) {
         return;
@@ -55,6 +57,7 @@ const result = (number) => {
         console.log(number);
         console.log(newExerciseHTML);
     }
+    selection.value = 0;
 }
 
 //event listener
